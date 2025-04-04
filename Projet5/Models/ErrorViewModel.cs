89f826fba0d76f704d7322c7dafd5038864899b9
Mickael Salvi <mickaelsalvi39@gmail.com>
2025-04-04 19:01:11 +0200

    Ajoutez des fichiers projet.

---
+++ Projet5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Projet5/Models/VoitureModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet5.Models
+{
+    public class VoitureModel
+    {
+        [Key]
+        public int Vin { get; set; }
+        [Required(ErrorMessage = "Le champ Marque est requis.")]
+        public string Marque { get; set; } = null!;
+        public string Modele { get; set; } = null!;
+        public int AnneeModel { get; set; }
+        public string Finition { get; set; }= null!;
+        [Display(Name = "Date d'achat")]
+        public DateOnly DateAchat { get; set; }
+        public DateOnly DateDisponibilité { get; set; }
+        public int PrixAchat { get; set; }
+        public int CoutReparation { get; set; }
+        
+        public bool EstDisponible { get; set; }
+        public string ImageUrl { get; set; }
+
+        // Autres propriétés spécifiques à votre modèle de voiture
+    }
+}
