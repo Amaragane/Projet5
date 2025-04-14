c1b61e2fa8dea931c8521c5ef56c6e1202ad00e0
Mickael Salvi <mickaelsalvi39@gmail.com>
2025-04-14 16:14:52 +0200

    Adding matadata to voituremodel and usermodel

---
+++ Projet5/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet5.Models
+{
+    public class UserMode
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Identifiant")]
+
+        [Required(ErrorMessage = "Le champ Identifiant est requis.")]
+        public string Identifiant { get; set; }
+        [Required(ErrorMessage = "Le champ Mot de passe est requis.")]
+        [Display(Name = "Mot de passe")]
+
+        public string MotDePasse { get; set; }
+        public bool EstAdministateur { get; set; }
+    }
+}
--- Projet5/Models/VoitureModel.cs
+++ Projet5/Models/VoitureModel.cs
@@ -6,18 +6,47 @@ namespace Projet5.Models
     {
         [Key]
         public int Vin { get; set; }
+
         [Required(ErrorMessage = "Le champ Marque est requis.")]
         public string Marque { get; set; } = null!;
+
+        [Required(ErrorMessage = "Le champ Modele est requis.")]
         public string Modele { get; set; } = null!;
+
+        [Display(Name = "Année du modele")]
+        [Required(ErrorMessage = "Le champ Année du modele est requis.")]
         public int AnneeModel { get; set; }
+
+        [Required(ErrorMessage = "Le champ Finition est requis.")]
         public string Finition { get; set; }= null!;
+
         [Display(Name = "Date d'achat")]
+        [Required(ErrorMessage = "Le champ Date d'achat est requis.")]
         public DateOnly DateAchat { get; set; }
+
+        [Display(Name = "Date de disponibilité")]
+        [Required(ErrorMessage = "Le champ date de disponibilité est requis.")]
         public DateOnly DateDisponibilité { get; set; }
+
+        [Required(ErrorMessage = "Le champ prix d'achat est requis.")]
+        [Display(Name = "Prix d'achat")]
         public int PrixAchat { get; set; }
+
+
+        [Display(Name = "Réparation")]
+        [Required(ErrorMessage = "Le champ des réparation est requis.")]
+        public int Reparation { get; set; }
+
+        [Display(Name = "Cout des répaaration")]
+        [Required(ErrorMessage = "Le champ Cout des réparation est requis.")]
         public int CoutReparation { get; set; }
-        
+
+        [Display(Name = "Disponibilité")]
+        [Required(ErrorMessage = "La disponibilité est requise.")]
         public bool EstDisponible { get; set; }
+
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "L'image est requise.")]
         public string ImageUrl { get; set; }
 
         // Autres propriétés spécifiques à votre modèle de voiture
